ad5b2bdd83d8e83eddc96347d0e1a7988225a019
Axel K M Ezaki <AxelKEza@hotmail.com>
2024-02-26 02:59:20 -0300

    projeto clone-buzzfeed terminado com questionário de super-herói ou vilão

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
+  public title: string = "";
+
+  public questions: any;
+  public questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = "";
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+
+      this.title = quiz_questions.title;
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  public onClickResponder(resposta: string){
+    this.answers.push(resposta);
+
+    this.nextStep();
+  }
+
+  private async nextStep(){
+    this.questionIndex += 1;
+
+    if(this.questionIndex < this.questionMaxIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const modaFinal = await this.resultado(this.answers);
+
+      this.answerSelected = quiz_questions.results[modaFinal as keyof typeof quiz_questions.results];
+      this.finished = true;
+    }
+  }
+  private async resultado(answers: string[]){
+    const result = answers.reduce((anterior, atual, _, array) => {
+      if(array.filter(item => item === anterior).length > array.filter(item => item === atual).length){
+        return anterior;
+      } else {
+        return atual;
+      }
+    });
+
+    return result;
   }
 
 }
